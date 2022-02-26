63c484d61219cd6a83cf0aeb23393b55a69b7778
Yasuo Fujita <mettoboshi.1108@gmail.com>
2022-02-26 20:28:29 +0900

    htmlを変数で返戻してみる

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -17,6 +17,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+$html = <<<EOF
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+  <ul>
+    <li>test</li>
+  </ul>
+</body>
+</html>
+EOF;
+
 Route::get('hello', function() {
   return '<html><body><h1>hello</h1></body></html>';
+});
+
+Route::get('return-html', function() use ($html) {
+  return $html;
 });
